652f17c493890853bcc9949d97b160840a86f90d
asheque33 <ashequrrahman3333@gmail.com>
2024-10-08 22:45:33 +0600

    refector donor leaderboard as responsive for  all device

--- src/pages/Leaderboard/Leaderboard.tsx
+++ src/pages/Leaderboard/Leaderboard.tsx
@@ -1,48 +1,77 @@
+import Container from "@/components/layouts/Container";
 import { useGetDonorsQuery } from "@/redux/features/donations/donorsApi";
 import { TDonationProps } from "@/types";
-import { Table } from "antd";
-
-const Leaderboard = () => {
+import { Table, Avatar, Typography, TableColumnsType } from "antd";
+const { Title } = Typography;
+const LeaderBoard = () => {
   const { data: donations, isFetching } = useGetDonorsQuery(undefined);
   const data = donations?.data?.map((item: TDonationProps) => {
     return {
-      _id: item._id,
       name: item.name,
+      image: <Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=8" />,
       donationAmount: item.amount,
     };
   });
-  const sortedDonors = data?.sort(
-    (a: { donationAmount: number }, b: { donationAmount: number }) =>
-      b.donationAmount - a.donationAmount
-  );
-  const columns = [
+  const sortedDonors = data
+    ?.sort(
+      (a: { donationAmount: number }, b: { donationAmount: number }) =>
+        b.donationAmount - a.donationAmount
+    )
+    ?.map((sortedItems: TDonationProps, index: number) => ({
+      ...sortedItems,
+      _id: index + 1,
+    }));
+  const columns: TableColumnsType<TDonationProps> = [
+    {
+      title: "Serial",
+      dataIndex: "_id",
+      key: "_id",
+      width: "20%",
+      responsive: ["xs", "sm", "md", "lg", "xl"],
+    },
+    {
+      title: "Image",
+      dataIndex: "image",
+      key: "image",
+      width: "25%",
+      responsive: ["xs", "sm", "md", "lg", "xl"],
+    },
     {
       title: "Name",
       dataIndex: "name",
       key: "name",
+      width: "25%",
+      responsive: ["xs", "sm", "md", "lg", "xl"],
     },
     {
       title: "Donation Amount",
       dataIndex: "donationAmount",
       key: "donationAmount",
+      width: "30%",
+      responsive: ["xs", "sm", "md", "lg", "xl"],
     },
   ];
   return (
-    <div>
-      <h2 className="font-bold text-lg md:text-3xl lg:text-5xl text-fuchsia-400 my-8 text-center">
-        Donors Leaderboard
-      </h2>
-      <div className="my-4 mx-8 border-4 border-fuchsia-400 rounded-xl">
-        <Table
-          className="bg-white ps-64 w-full"
+    <Container>
+      <Typography className="my-6 text-center">
+        <Title level={4}>
+          {" "}
+          <span className="text-[#FF4C4E]">Donors</span> LeaderBoard
+        </Title>
+      </Typography>
+      <div className="my-4 overflow-x-auto border border-[#ff4c4e] rounded-xl">
+        <Table<TDonationProps>
+          className="bg-white  w-full"
           loading={isFetching}
           columns={columns}
           dataSource={sortedDonors}
+          size="large"
           rowKey={(record) => record._id}
+          scroll={{ x: 500 }}
         />
       </div>
-    </div>
+    </Container>
   );
 };
 
-export default Leaderboard;
+export default LeaderBoard;
