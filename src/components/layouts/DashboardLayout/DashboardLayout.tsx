4309d085938efedd605da0b45480ac6188bbbab9
asheque33 <ashequrrahman3333@gmail.com>
2024-10-15 22:56:31 +0600

    redirect dashboard to dashboard/home

--- src/components/layouts/DashboardLayout/DashboardLayout.tsx
+++ src/components/layouts/DashboardLayout/DashboardLayout.tsx
@@ -1,20 +1,47 @@
 import { Layout } from "antd";
-import Sidebar from "../Sidebar";
-import { Outlet, useLocation } from "react-router-dom";
-import Navbar from "../Navbar";
+import Sidebar from "../Shared/Sidebar";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import Navbar from "../Shared/Navbar";
+import { useEffect, useState } from "react";
 const { Content } = Layout;
 
 const DashboardLayout = () => {
   const location = useLocation();
+  const navigate = useNavigate();
+  const [isSidebarCollapsed, setIsSidebarCollapsed] = useState(true);
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 520) {
+        setIsSidebarCollapsed(false);
+      } else {
+        setIsSidebarCollapsed(true);
+      }
+    };
+    window.addEventListener("resize", handleResize);
+    handleResize();
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+  useEffect(() => {
+    if (location.pathname === "/dashboard") {
+      navigate("/dashboard/home");
+    }
+  }, [location, navigate]);
 
   const isDashboardActive = location.pathname === "/dashboard";
 
   return (
     <Layout>
-      {!isDashboardActive ? <Navbar /> : ""}
+      {!isDashboardActive ? <Navbar /> : null}
       <Layout>
-        <Sidebar />
-        <Content>
+        <Sidebar isDashboardActive={isDashboardActive} />
+        <Content
+          style={{
+            marginLeft: isSidebarCollapsed ? 0 : 0, // Ensures the content does not shift when the sidebar toggles
+            minHeight: `calc(100vh - 64px)`,
+            // padding: "24px",
+            // transition: "margin-left 0.3s ease", // Smooth transition if needed
+          }}
+        >
           <Outlet />
         </Content>
       </Layout>
