8003862a450d2326f0c2d5039b9077b2922b857a
asheque33 <ashequrrahman3333@gmail.com>
2024-10-24 12:28:55 +0600

    added darkTheme/lightTheme in Dashboard Layout

--- src/components/layouts/DashboardLayout/DashboardLayout.tsx
+++ src/components/layouts/DashboardLayout/DashboardLayout.tsx
@@ -1,13 +1,15 @@
-import { Layout } from "antd";
+import { ConfigProvider, Layout } from "antd";
 import Sidebar from "../Shared/Sidebar";
 import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import Navbar from "../Shared/Navbar";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import { ThemeContext } from "@/components/ThemeContext/ThemeProvider";
 const { Content } = Layout;
 
 const DashboardLayout = () => {
   const location = useLocation();
   const navigate = useNavigate();
+  const { theme } = useContext(ThemeContext);
   const [isSidebarCollapsed, setIsSidebarCollapsed] = useState(true);
   useEffect(() => {
     const handleResize = () => {
@@ -30,22 +32,52 @@ const DashboardLayout = () => {
   const isDashboardActive = location.pathname === "/dashboard";
 
   return (
-    <Layout>
-      {!isDashboardActive ? <Navbar /> : null}
+    <ConfigProvider
+      theme={
+        theme === "dark"
+          ? {
+              token: {
+                colorPrimary: "#ff4c4e",
+                colorTextSecondary: "#3c3d37",
+                // colorBgMask: "#f5efe6",
+                colorBgLayout: "#3c3d37", //bodyBg
+                colorBgBase: "#313131", //cardBg,ButtonBg
+                colorBgSolid: "#001629",
+                //"#313131",
+                //navbar,footer
+                colorText: "#f0f0f0",
+              }, //dark theme
+            }
+          : {
+              token: {
+                colorPrimary: "#ff4c4e",
+                colorTextSecondary: "#fff",
+                // colorBgMask: "#3c3d37",
+                colorBgLayout: "#f5f5f5",
+                colorBgBase: "#fff", //cardBg,ButtonBg
+                colorBgSolid: "#f5efe6",
+                colorText: "#3c3d37",
+              }, //default/light theme
+            }
+      }
+    >
       <Layout>
-        <Sidebar isDashboardActive={isDashboardActive} />
-        <Content
-          style={{
-            marginLeft: isSidebarCollapsed ? 0 : 0, // Ensures the content does not shift when the sidebar toggles
-            minHeight: `calc(100vh - 64px)`,
-            // padding: "24px",
-            // transition: "margin-left 0.3s ease", // Smooth transition if needed
-          }}
-        >
-          <Outlet />
-        </Content>
+        {!isDashboardActive ? <Navbar /> : null}
+        <Layout>
+          <Sidebar isDashboardActive={isDashboardActive} />
+          <Content
+            style={{
+              marginLeft: isSidebarCollapsed ? 0 : 0, // Ensures the content does not shift when the sidebar toggles
+              minHeight: `calc(100vh - 64px)`,
+              // padding: "24px",
+              // transition: "margin-left 0.3s ease", // Smooth transition if needed
+            }}
+          >
+            <Outlet />
+          </Content>
+        </Layout>
       </Layout>
-    </Layout>
+    </ConfigProvider>
   );
 };
 
